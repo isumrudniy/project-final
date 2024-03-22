22a8b14efafd6503646879bb3a5753ada006128d
darkisu <isumrudniy@me.com>
2024-03-22 10:00:39 +0500

    * Completed tasks 1,2,3,6

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -7,10 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,15 +22,22 @@ public class FileUtil {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
+        Path directory = Paths.get(directoryPath);
+        if (!Files.exists(directory)) {
+            try {
+                Files.createDirectories(directory);
+            } catch (IOException e) {
+                throw new IllegalRequestDataException("Failed to create directory: " + directoryPath);
             }
         }
+
+        Path filePath = directory.resolve(fileName);
+        try {
+            Files.write(filePath, multipartFile.getBytes());
+        } catch (IOException e) {
+            throw new IllegalRequestDataException("Failed to upload file: " + fileName);
+        }
+
     }
 
     public static Resource download(String fileLink) {

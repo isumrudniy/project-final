197bbb8a6e62df2756f1165d9aed7670269a7179
isumrudniy <isumrudniy@yandex.ru>
2024-04-06 16:54:14 +0500

    * Task 4 completed

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -9,24 +9,24 @@ from PROFILE;
 delete
 from ACTIVITY;
 alter
-sequence ACTIVITY_ID_SEQ restart with 1;
+    sequence ACTIVITY_ID_SEQ restart with 1;
 delete
 from TASK;
 alter
-sequence TASK_ID_SEQ restart with 1;
+    sequence TASK_ID_SEQ restart with 1;
 delete
 from SPRINT;
 alter
-sequence SPRINT_ID_SEQ restart with 1;
+    sequence SPRINT_ID_SEQ restart with 1;
 delete
 from PROJECT;
 alter
-sequence PROJECT_ID_SEQ restart with 1;
+    sequence PROJECT_ID_SEQ restart with 1;
 
 delete
 from USERS;
 alter
-sequence USERS_ID_SEQ restart with 1;
+    sequence USERS_ID_SEQ restart with 1;
 
 insert into USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, DISPLAY_NAME)
 values ('user@gmail.com', '{noop}password', 'userFirstName', 'userLastName', 'userDisplayName'),
@@ -48,12 +48,13 @@ insert into PROFILE (ID, LAST_FAILED_LOGIN, LAST_LOGIN, MAIL_NOTIFICATIONS)
 values (1, null, null, 49),
        (2, null, null, 14);
 
-insert into CONTACT (ID, CODE, VALUE)
+insert into CONTACT (ID, CODE, `VALUE`)
 values (1, 'skype', 'userSkype'),
        (1, 'mobile', '+01234567890'),
        (1, 'website', 'user.com'),
        (2, 'github', 'adminGitHub'),
-       (2, 'tg', 'adminTg');
+       (2, 'tg', 'adminTg'),
+       (2, 'vk', 'adminVk');
 
 
 insert into PROJECT (code, title, description, type_code, parent_id)
